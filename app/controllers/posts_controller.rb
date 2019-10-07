32552523f73461e0b250ed7c2be61c05e0f41baa
birnamwood <birnamwood19@gmail.com>
2019-10-07 19:02:17 +0900

    [Update]記事にタグ追加機能を導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def new
     @post = Post.new
+    @post_tag = @post.post_tags.build
+    @post_image = @post.post_images.build
     @end_user = current_end_user
   end
 
@@ -25,6 +27,8 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:zipcode, :prefecture_id, :municipality_id, :address, :post_title, :post_name, :overview, :video, :acccess, :budget, :body)
+    params.require(:post).permit(:zipcode, :prefecture_id, :municipality_id, :address, :post_title, :post_name, :overview, :video, :acccess, :budget, :body,
+              post_tags_attributes: [:id, :post_id, :post_image_id, :post_image_comment, :_destroy],
+              post_images_attributes: [:id, :post_id, :tag_id, :_destroy])
   end
 end
