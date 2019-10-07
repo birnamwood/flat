f3cbe85d4c06ed11493a869b7a65bf5c81b8c8f2
birnamwood <birnamwood19@gmail.com>
2019-10-07 18:00:18 +0900

    [Add]投稿詳細ページ作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,21 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
+    @user = @post.end_user
+    @comment_new = Comment.new
+    @comments = @post.comments
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.user_id = current_user.id
+
+    @post.save
   end
 
   private
   def post_params
-    params.require(:post).permit(:video)
-   #params.require(:モデル名).permit(:カラム名)
+    params.require(:post).permit(:zipcode, :prefecture_id, :municipality_id, :address, :post_title, :post_name, :overview, :video, :acccess, :budget, :body)
   end
 end
