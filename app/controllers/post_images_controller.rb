d8c7eb2a37a068a61531902445282464c1c503f1
birnamwood <birnamwood19@gmail.com>
2019-10-13 02:27:13 +0900

    [Update]タグの削除処理修正、画像の削除処理作成

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,2 +1,12 @@
 class PostImagesController < ApplicationController
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    if @post_image.destroy
+      flash[:success] = "画像を削除しました。"
+    else
+      flash[:danger] = "画像の削除に失敗しました。"
+    end
+
+    redirect_to edit_post_path(@post_image.post)
+  end
 end
--- app/controllers/post_tags_controller.rb
+++ app/controllers/post_tags_controller.rb
@@ -3,7 +3,7 @@ class PostTagsController < ApplicationController
   def destroy
     @post_tag = PostTag.find(params[:id])
     if @post_tag.destroy
-      flash[:success] = "タグを削除"
+      flash[:success] = "タグを削除しました。"
     end
 
     redirect_to edit_post_path(@post_tag.post)
