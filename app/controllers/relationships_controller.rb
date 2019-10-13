b75e8dcf3ccfda3f1fe3e01f5b36f1fec5452215
birnamwood <birnamwood19@gmail.com>
2019-10-14 05:23:46 +0900

    [Update]フォロー機能を非同期に変更

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,19 @@
 class RelationshipsController < ApplicationController
   def create
-    end_user = EndUser.find(params[:followed_id])
-    current_end_user.follow(end_user)
-    redirect_to end_user
+    @end_user = EndUser.find(params[:followed_id])
+    current_end_user.follow(@end_user)
+    respond_to do |format|
+      format.html { redirect_to end_user_path(@end_user) }
+      format.js
+    end
   end
 
   def destroy
-    end_user = Relationship.find(params[:id]).followed
-    current_end_user.unfollow(end_user)
-    redirect_to end_user
+    @end_user = Relationship.find(params[:id]).followed
+    current_end_user.unfollow(@end_user)
+    respond_to do |format|
+      format.html { redirect_to end_user_path(@end_user) }
+      format.js
+    end
   end
 end
