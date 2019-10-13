f5c466d29c7a0c431f75e0489745e4189cea8082
birnamwood <birnamwood19@gmail.com>
2019-10-14 04:56:39 +0900

    [Update]行った場所保存機能を非同期に変更

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,21 +1,21 @@
 class VisitsController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
-    visit = current_end_user.visits.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    visit = current_end_user.visits.new(post_id: @post.id)
     visit.save
     respond_to do |format|
-      format.html { redirect_to post_path(post) }
+      format.html { redirect_to post_path(@post) }
       format.js
     end
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    visit = current_end_user.visits.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    visit = current_end_user.visits.find_by(post_id: @post.id)
     visit.destroy
     respond_to do |format|
-      format.html { redirect_to post_path(post) }
+      format.html { redirect_to post_path(@post) }
       format.js
     end
   end
