d31d703f8bbcbdd08823c63e3cddd8870580b8ac
birnamwood <birnamwood19@gmail.com>
2019-10-16 16:49:47 +0900

    [Update]userのnicknameの登録がなければnameを表示するように変更

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,6 @@
 class SearchesController < ApplicationController
   def index
     @posts = Post.includes([:end_user]).page(params[:page]).reverse_order
-    @check = params[:q][:post_name_or_end_user_nickname_cont]
+    @check = params[:q][:post_name_or_end_user_nickname_or_end_user_name_cont]
   end
 end
