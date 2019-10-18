e26bbfd2c41c5131ec2e01ee620baf3382688ea7
birnamwood <birnamwood19@gmail.com>
2019-10-18 22:20:10 +0900

    [Add]訪問数とクリップ数ランキング作成

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -11,6 +11,8 @@ class RankingsController < ApplicationController
 
     # ランキング
     @user_ranking = EndUser.find(Post.group(:end_user_id).order('count(end_user_id) desc').limit(10).pluck(:end_user_id))
+    @clip_ranking = Post.find(Clip.group(:post_id).order('count(post_id) desc').limit(10).pluck(:post_id))
+    @visit_ranking = Post.find(Visit.group(:post_id).order('count(post_id) desc').limit(10).pluck(:post_id))
 
   end
 end
