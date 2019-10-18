67bb1d2ed31c9df2684a0ddb4a3051ca2c732642
birnamwood <birnamwood19@gmail.com>
2019-10-18 17:21:18 +0900

    [Add]都道府県別投稿数グラフ作成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery.raty
 //= require cocoon
 //= require materialize
+//= require Chart.min
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,4 +1,16 @@
 class RankingsController < ApplicationController
   def index
+    @prefectures =  Prefecture.all
+    @labelline = Array.new
+    @prefectures.each do |g|
+        @labelline.push(g.prefecture_name)
+    end
+
+    @prefectures =  Prefecture.all
+    @score = Array.new
+    @prefectures.each do |g|
+        @score.push(g.posts.count)
+    end
+
   end
 end
