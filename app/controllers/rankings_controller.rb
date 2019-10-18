da1222a96ea561d0e8e7156c36ee3301bf5cfbb1
birnamwood <birnamwood19@gmail.com>
2019-10-18 19:15:41 +0900

    [Add]ユーザー投稿数ランキング追加

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,5 +1,6 @@
 class RankingsController < ApplicationController
   def index
+    # グラフ用
     @prefectures =  Prefecture.all
     @labelline = Array.new
     @score = Array.new
@@ -7,6 +8,9 @@ class RankingsController < ApplicationController
         @labelline.push(g.prefecture_name)
         @score.push(g.posts.count)
     end
+
+    # ランキング
+    @user_ranking = EndUser.find(Post.group(:end_user_id).order('count(end_user_id) desc').limit(10).pluck(:end_user_id))
 
   end
 end
