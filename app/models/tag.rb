3b5cb6dd3a66845f1677a7c2c918986f343ca861
birnamwood <birnamwood19@gmail.com>
2019-10-20 20:42:43 +0900

    [Add]Tagモデルにバリデーション追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,6 @@ class Tag < ApplicationRecord
 
   has_many :post_in_tags, foreign_key: "tag_id", class_name: "PostTag", dependent: :destroy
   has_many :tag_posts, through: :post_in_tags, source: :post
+
+  validates :tag_name, presence: true
 end
