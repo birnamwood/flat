535aec2e60d8f9615f86cb562e5178331f03a0e2
birnamwood <birnamwood19@gmail.com>
2019-10-22 15:52:18 +0900

    [Update]ユーザーコントローラテスト追加

--- spec/features/end_user_spec.rb
+++ spec/features/end_user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "EndUserに関するテスト", type: :feature do
+RSpec.feature "EndUserコントローラ関連", type: :feature do
   let(:region) {FactoryBot.create(:region)}
   let(:prefecture) {FactoryBot.create(:prefecture, region_id: region.id)}
   let(:municipality) {FactoryBot.create(:municipality, prefecture_id: prefecture.id)}
@@ -26,10 +26,9 @@ RSpec.feature "EndUserに関するテスト", type: :feature do
         visit end_user_path(@user1)
         expect(page).to have_content @user1.nickname
         expect(page).to have_link "",href: edit_end_user_path(@user1)
+        expect(page).to have_no_button "フォローする"
         expect(page).to have_link "",href: following_end_user_path(@user1)
         expect(page).to have_link "",href: followers_end_user_path(@user1)
-        expect(page).to have_link "",href: "/relationships"
-        expect(page).to have_link "",href: relationship_path(@user1)
         expect(page).to have_selector "img"
 
         @user1.posts.each do |post|
@@ -37,14 +36,14 @@ RSpec.feature "EndUserに関するテスト", type: :feature do
         end
       end
 
-      scenario "自分の詳細ページでno-imageの画像が表示されているか" do
-        visit end_user_path(@user1)
-        expect(page).to have_selector "img"
-      end
-
       scenario "他人の詳細ページの表示内容とリンク" do
         visit end_user_path(@user2)
         expect(page).to have_content @user2.nickname
+        expect(page).to have_no_link "",href: edit_end_user_path(@user2)
+        expect(page).to have_button "フォローする"
+        expect(page).to have_link "",href: following_end_user_path(@user2)
+        expect(page).to have_link "",href: followers_end_user_path(@user2)
+        expect(page).to have_selector "img"
 
         @user2.posts.each do |post|
           expect(page).to have_link post.post_name,href: post_path(post)
@@ -55,14 +54,12 @@ RSpec.feature "EndUserに関するテスト", type: :feature do
     feature "自分のプロフィールの更新" do
       before do
         visit edit_end_user_path(@user1)
-        find_field('end_user[name]').set('updated_name')
         find_field('end_user[nickname]').set('updated_nickname')
         find("input[name='commit']").click
       end
       scenario "userが更新されているか" do
-        expect(page).to have_content "updated_name"
         expect(page).to have_content "updated_nickname"
-        expect(User.find(1).profile_image_id).to be_truthy
+        expect(EndUser.find(1).nickname).to be_truthy
       end
       scenario "リダイレクト先は正しいか" do
         expect(page).to have_current_path end_user_path(@user1)
@@ -81,15 +78,15 @@ RSpec.feature "EndUserに関するテスト", type: :feature do
 
     feature "有効ではない内容のuserの更新" do
       before do
-        visit edit_user_path(@user1)
-        find_field('user[name]').set(nil)
+        visit edit_end_user_path(@user1)
+        find_field('end_user[nickname]').set(nil)
         find("input[name='commit']").click
       end
       scenario "リダイレクト先が正しいか" do
-        expect(page).to have_current_path user_path(@user1)
+        expect(page).to have_current_path edit_end_user_path(@user1)
       end
       scenario "エラーメッセージが表示されるか" do
-        expect(page).to have_content "error"
+        expect(page).to have_content "ユーザー情報の更新に失敗しました。"
       end
     end
   end
